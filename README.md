38a57d87f4b32ff220ac5e3b53095fe3d3a5f463
lyndontavares <integraldominio@gmail.com>
2019-11-28 09:03:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-generic-mat-table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-generic-mat-table)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Car } from "./resources";
+import { Observable } from "rxjs";
+import { DeserializeArray } from "cerialize";
+import { JsonArray } from "cerialize/dist/util";
+import { map, tap } from "rxjs/operators";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit {
+  cars$: Observable<Car[]>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.cars$ = this.http.get("/assets/car.json").pipe(
+      map((res: JsonArray) => DeserializeArray(res, Car)),
+      tap(res => console.log(res))
+    );
+  }
+}
